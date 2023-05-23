9abc7b9f7ed5a014db296ac5460c0af7a33c52b8
student <a.tsymbaliuk2007@gmail.com>
2023-05-23 19:14:48 +0300

    факторіал числа 5

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ try:
     print(10/0)
 except Exception:
     logging.exception('gg bro')
+def factorial(n):
+    logging.info(f"ропочато обчислення факторіала числа {n}")
+    result = 1
+    for i in range(1,n+1):
+        result *= i
+    logging.info(f'завершення обчислення факторіалу числа {n} езультат {result}')
+    return result
+logging.basicConfig(level=logging.INFO)
+print(factorial(5))
